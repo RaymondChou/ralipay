d1578610eb776a1bb35c08dcfef170898c9c2d33
zhouyt <zhouyt@iiseeuu.com>
2013-03-21 15:53:22 +0800

    add rsa and more ...

--- lib/ralipay/common.rb
+++ lib/ralipay/common.rb
@@ -1,5 +1,7 @@
 module Common
 
+  require 'openssl'
+
   #生成签名结果
   def self.build_sign(data_array, sign_type = 'RSA', rsa_private_key_path = '')
     #把数组所有元素，按照“参数=参数值”的模式用“&”字符拼接成字符串
@@ -10,7 +12,7 @@ module Common
     elsif sign_type == 'MD5'
       return self.md5_sign(for_sign_string)
     else
-      puts 'Unknown sign_type!'
+      fail('Unknown sign_type!')
     end
   end
 
@@ -29,14 +31,30 @@ module Common
   #RSA签名
   def self.rsa_sign(for_sign_string,rsa_private_key_path)
     #读取私钥文件
-    #转换为openssl密钥，必须是没有经过pkcs8转换的私钥
-    #调用openssl内置签名方法，生成签名
+    rsa_private_key_file = File.read(rsa_private_key_path)
+    #转换为openssl密钥
+    openssl_key = OpenSSL::PKey::RSA.new rsa_private_key_file
+    #使用openssl方法进行sha1签名digest(不能用sha256)
+    digest = OpenSSL::Digest::SHA1.new
+    signature = openssl_key.sign digest, for_sign_string
     #base64编码
+    signature = Base64.encode64(signature)
+    #signature = signature.gsub!("\n",'')
+    return signature
   end
 
   #MD5签名
   def self.md5_sign(for_sign_string)
 
+  end
+
+  #验签
+  def verify?(for_sign_string, signed_string, rsa_public_key_path)
+    #读取公钥文件
+    rsa_public_key_file = File.read(rsa_public_key_path)
+    openssl_public = OpenSSL::PKey::RSA.new rsa_public_key_file
+    digest = OpenSSL::Digest::SHA1.new
+    openssl_public.verify(digest, Base64.decode64(signed_string), for_sign_string)
   end
 
 end
