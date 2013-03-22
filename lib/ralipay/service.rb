6f56dce8064629e373f3d330473f53e32ef36557
zhouyt <zhouyt@iiseeuu.com>
2013-03-22 17:48:55 +0800

    @todo alipay_wap_trade_create_direct

--- lib/ralipay/service.rb
+++ lib/ralipay/service.rb
@@ -66,8 +66,8 @@ class Service
   #创建alipay.wap.trade.create.direct接口
   def alipay_wap_trade_create_direct parameter
     #除去数组中的空值和签名参数
+    parameter[:req_data] = CGI::escape parameter[:req_data]
     @@parameter = Ralipay::Common::para_filter parameter
-    parameter[:req_data]  = CGI::escape parameter[:req_data]
     @@format    = parameter[:format]
     sort_array  = @@parameter.sort
     #生成签名
@@ -77,7 +77,7 @@ class Service
                + '&sign='                                              \
                + CGI::escape(@@my_sign)
     #@@req_data = CGI::escape(@@req_data)
-
+    puts @@req_data
     #请求支付宝接口
     uri  = URI.parse (@@gateway_order)
     http = Net::HTTP.new uri.host, uri.port
@@ -92,7 +92,6 @@ class Service
   def parse_token string
     unsecaped_string = CGI::unescape string
     unsecaped_string = unsecaped_string.split('&')
-    p unsecaped_string
   end
 
 end
